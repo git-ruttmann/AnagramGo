fcc42af15393cf0e8fcf68cf669141293375de88
Ruttmann <matthias@ruttmann.name>
2021-02-28 21:58:54 +0100

    Refactor word analysis process

--- anagram/scanner.go
+++ anagram/scanner.go
@@ -4,125 +4,146 @@ const threadCount = 8
 
 // Scanner scans words
 type Scanner struct {
-	options      *Options
-	anagram      *Anagram
-	storage      *Storage
 	wordAnalyzer wordAnalyzer
-	endChannel   chan bool
-	endRequested bool
-	reporter     func(string)
+	wordCombiner wordCombiner
 }
 
 type wordAnalyzer struct {
-	texts chan string
-	words chan Word
+	options *Options
+	anagram *Anagram
+	texts   chan string
+	words   chan Word
+}
+
+type wordCombiner struct {
+	storage         *Storage
+	options         *Options
+	anagram         *Anagram
+	threads         map[int]*scanThread
+	stageTwoThreads map[int]*scanThread
+	processedWordID int
+	stageOne        chan int
+	words           chan Word
+	endChannel      chan bool
+	endRequested    bool
+	reporter        func(string)
 }
 
 // NewScanner initalizes a new scanner
 func NewScanner(anagram *Anagram, options *Options, reporter func(string)) (s *Scanner) {
 	s = &Scanner{
-		anagram:  anagram,
-		options:  options,
-		reporter: reporter,
+		wordCombiner: wordCombiner{
+			anagram:  anagram,
+			options:  options,
+			reporter: reporter,
+		},
+		wordAnalyzer: wordAnalyzer{
+			anagram: anagram,
+			options: options,
+		},
 	}
 
-	s.storage = InitStorage(anagram, options)
-	s.endChannel = make(chan bool, 1)
+	s.wordCombiner.storage = InitStorage(anagram, options)
+	s.wordCombiner.endChannel = make(chan bool, 1)
+
 	s.wordAnalyzer.texts = make(chan string, 20)
 	s.wordAnalyzer.words = make(chan Word, 20)
 
-	go s.processAcceptedWords()
-	go s.scanWords()
+	s.wordCombiner.words = s.wordAnalyzer.words
+
+	go s.wordCombiner.combineAcceptedWords()
+	go s.wordAnalyzer.scanWords()
 	return
 }
 
-func (s *Scanner) scanWords() {
-	for text := range s.wordAnalyzer.texts {
-		if len(text) > s.options.MinimumLength {
-			if word := s.anagram.Combine(text); word != nil {
-				s.wordAnalyzer.words <- *word
+func (w *wordAnalyzer) scanWords() {
+	for text := range w.texts {
+		if len(text) > w.options.MinimumLength {
+			if word := w.anagram.Combine(text); word != nil {
+				w.words <- *word
 			}
 		}
 	}
 
-	close(s.wordAnalyzer.words)
+	close(w.words)
 }
 
-func (s *Scanner) processAcceptedWords() {
+func (w *wordCombiner) combineAcceptedWords() {
 	wordID := 0
-	nextProcessedWordID := 1
+	w.processedWordID = 1
 	generateID := func() int {
 		wordID++
 		return wordID
 	}
 
-	stageOne := make(chan int, 1)
-	stageTwo := make(chan int, 1)
-	threads := make(map[int]*scanThread)
-	completedStageTwo := make(map[int]*scanThread)
-	wordChannel := s.wordAnalyzer.words
+	w.stageOne = make(chan int, 1)
+	w.threads = make(map[int]*scanThread)
+	w.stageTwoThreads = make(map[int]*scanThread)
+	wordChannel := w.words
 
 	for {
 		select {
-		case id := <-stageTwo:
-			t := threads[id]
-			delete(threads, id)
-			t.HandleCompletionInSynchronizationThread(s.reporter)
-
-		case id := <-stageOne:
-			t := threads[id]
-			completedStageTwo[id] = t
+		case id := <-w.stageOne:
+			w.stageTwoThreads[id] = w.threads[id]
 
 		case word, ok := <-wordChannel:
 			if ok {
-				id := generateID()
-				t := newScanThread(s.storage, &word)
-				threads[id] = t
-				t.StageOne()
-				go t.Scan(id, stageOne)
+				w.launchNewThread(&word, generateID())
 			} else {
-				s.wordAnalyzer.words = nil
+				w.words = nil
+				wordChannel = nil
 			}
 		}
 
 		for {
-			if t, ok := completedStageTwo[nextProcessedWordID]; ok {
-				id := nextProcessedWordID
-				nextProcessedWordID++
-
-				delete(completedStageTwo, id)
-				delete(threads, id)
-				t.StageTwo()
-
-				// lots to do - start scanning for each thread in completed stage
-				if len(t.complete.parts) > 1000 {
-					for nid, nt := range completedStageTwo {
-						nt.StageTwo()
-						go nt.Scan(nid, stageOne)
-					}
-
-					completedStageTwo = make(map[int]*scanThread)
-				}
-
-				t.Scan2(id)
-				t.HandleCompletionInSynchronizationThread(s.reporter)
+			if t, ok := w.stageTwoThreads[w.processedWordID]; ok {
+				w.runStageTwo(t, w.processedWordID)
+				w.processedWordID++
 			} else {
 				break
 			}
 		}
 
-		// throttle number of parallel processed words
-		if len(threads) > 3000 {
+		if len(w.threads) > 2000 {
 			wordChannel = nil
 		} else {
-			wordChannel = s.wordAnalyzer.words
+			wordChannel = w.words
 		}
 
-		// finish the processing function
-		if s.endRequested && len(threads) == 0 && s.wordAnalyzer.words == nil {
-			s.endChannel <- true
+		if w.endRequested && len(w.threads) == 0 && w.words == nil {
+			w.endChannel <- true
 			break
 		}
+	}
+}
+
+func (w *wordCombiner) launchNewThread(word *Word, id int) {
+	t := newScanThread(w.storage, word)
+	w.threads[id] = t
+	t.StageOne()
+	go t.Scan(id, w.stageOne)
+}
+
+func (w *wordCombiner) runStageTwo(t *scanThread, id int) {
+	delete(w.stageTwoThreads, id)
+	delete(w.threads, id)
+	t.StageTwo()
+
+	// lots to do - start scanning for each thread in completed stage
+	if len(t.complete.parts) > 1000 {
+		w.relaunchStageOneForAllThreads()
+	}
+
+	t.Scan2(id)
+	t.HandleCompletionInSynchronizationThread(w.reporter)
+}
+
+func (w *wordCombiner) relaunchStageOneForAllThreads() {
+	oldMap := w.stageTwoThreads
+	w.stageTwoThreads = make(map[int]*scanThread)
+	for id, t := range oldMap {
+		t.StageTwo()
+		go t.Scan(id, w.stageOne)
 	}
 }
 
@@ -134,6 +155,6 @@ func (s *Scanner) ProcessWord(text string) {
 // Final closed the scanner, call after the last word
 func (s *Scanner) Final() {
 	close(s.wordAnalyzer.texts)
-	s.endRequested = true
-	<-s.endChannel
+	s.wordCombiner.endRequested = true
+	<-s.wordCombiner.endChannel
 }
